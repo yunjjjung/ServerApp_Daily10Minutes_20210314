9d8f7a014c6d0bd96f9cf4ad4128081d02eb4891
조경진 <cho881020@gmail.com>
2021-03-14 16:10:48 +0900

    POST - FormData에 로그인 필요 데이터 첨부

--- app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/utils/ServerUtil.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/utils/ServerUtil.kt
@@ -1,5 +1,7 @@
 package com.tjoeun.serverapp_daily10minutes_20210314.utils
 
+import okhttp3.FormBody
+
 class ServerUtil {
 
 //    API를 호출해주는 함수들을 모아두기 위한 클래스. (코드 정리 차원)
@@ -17,12 +19,18 @@ class ServerUtil {
 
 //        함수 작성 - 로그인 기능 담당 함수.
 
-        fun postRequestLogin() {
+        fun postRequestLogin(id : String, pw : String) {
 
 //            실제 기능 수행 주소 ex. 로그인 - http://15.164.153.174/user
 //            HOST_URL/user => 최종 주소 완성.
 
             val urlString = "${HOST_URL}/user"
+
+//            POST 메쏘드 - formBody에 데이터 첨부.
+            val formData = FormBody.Builder()
+                .add("email", id)
+                .add("password", pw)
+                .build()
 
 
         }
