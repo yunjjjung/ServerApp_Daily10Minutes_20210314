ec194c64e94e104ec2b78f18261e0f18db2a7dca
조경진 <cho881020@gmail.com>
2021-03-14 16:14:46 +0900

    요청 정보를 종합하는 변수 request 생성

--- app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/utils/ServerUtil.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/utils/ServerUtil.kt
@@ -1,6 +1,7 @@
 package com.tjoeun.serverapp_daily10minutes_20210314.utils
 
 import okhttp3.FormBody
+import okhttp3.Request
 
 class ServerUtil {
 
@@ -30,6 +31,12 @@ class ServerUtil {
             val formData = FormBody.Builder()
                 .add("email", id)
                 .add("password", pw)
+                .build()
+
+//            API 요청(Request)을 어디로 어떻게 할건지 종합하는 변수.
+            val request = Request.Builder()
+                .url(urlString) // 어디로 가는지?
+                .post(formData) // POST방식 - 필요데이터(formData) 들고 가도록
                 .build()
 
 
