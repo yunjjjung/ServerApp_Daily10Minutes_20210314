81da60d918e8624d71417c973b9e1da31f92979b
조경진 <cho881020@gmail.com>
2021-03-14 15:35:35 +0900

    버튼 클릭시 아이디/비번 추출

--- app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/MainActivity.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/MainActivity.kt
@@ -2,6 +2,7 @@ package com.tjoeun.serverapp_daily10minutes_20210314
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -13,6 +14,16 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        loginBtn.setOnClickListener {
+
+//            입력한 아이디 / 비번을 변수로 저장
+            val inputId = emailEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+
+//            이 아이디/비번이 회원이 맞는지, 서버에 확인 요청. => 로그인 요청.
+
+        }
 
     }
 
