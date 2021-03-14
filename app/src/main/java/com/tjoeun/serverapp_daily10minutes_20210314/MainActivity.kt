de0b0e7402b18c1fa4158cbd3612294e81e37778
조경진 <cho881020@gmail.com>
2021-03-14 15:42:49 +0900

    OkHttp 라이브러리 설치 + ServerUtil 클래스 추가

--- app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/MainActivity.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/MainActivity.kt
@@ -23,6 +23,8 @@ class MainActivity : BaseActivity() {
 
 //            이 아이디/비번이 회원이 맞는지, 서버에 확인 요청. => 로그인 요청.
 
+
+
         }
 
     }
---
+++ app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/utils/ServerUtil.kt
@@ -0,0 +1,7 @@
+package com.tjoeun.serverapp_daily10minutes_20210314.utils
+
+class ServerUtil {
+
+//    API를 호출해주는 함수들을 모아두기 위한 클래스. (코드 정리 차원)
+
+}
