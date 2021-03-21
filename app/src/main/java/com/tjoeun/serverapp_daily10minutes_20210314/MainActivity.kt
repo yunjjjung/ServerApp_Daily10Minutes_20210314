9f771275f46c2bb8a95a57005fbf866976104d61
조경진 <cho881020@gmail.com>
2021-03-21 13:54:26 +0900

    프로젝트목록 추가작업 - 서버에서 받아오자.

--- app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/MainActivity.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/MainActivity.kt
@@ -2,8 +2,11 @@ package com.tjoeun.serverapp_daily10minutes_20210314
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tjoeun.serverapp_daily10minutes_20210314.datas.Project
 
 class MainActivity : BaseActivity() {
+
+    val mProjectList = ArrayList<Project>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +20,11 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        메인화면에 들어오면 => 프로젝트 목록이 뭐뭐있는지 서버에 요청 (ServerUtil 함수 추가)
+//        받아온 결과를 분석해서 => Project() 형태로 만들어서 => mProjectList에 add해주자.
+
+
 
     }
 
