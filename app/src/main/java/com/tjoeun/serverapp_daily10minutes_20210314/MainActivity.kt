6db6dd1cf1b502ada585983a11ae7f6f9292ef21
조경진 <cho881020@gmail.com>
2021-03-21 09:53:19 +0900

    runOnUiThread - UI동작 전용 쓰레드에서 토스트 띄우기

--- app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/MainActivity.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/MainActivity.kt
@@ -45,8 +45,12 @@ class MainActivity : BaseActivity() {
 
                         val message = json.getString("message")
 
-//                        토스트를 띄우려고 하면 앱이 죽는다.
-                        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+//                       (백그라운드 쓰레드에서) 토스트를 띄우려고 하면 앱이 죽는다.
+//                        UI 동작 관련 코드는, 반드시 UI 쓰레드에서만 동작시켜야 함.
+
+                        runOnUiThread {
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                        }
 
                     }
 
