e98fc3c640beafeb4013315cb36b45468093479f
조경진 <cho881020@gmail.com>
2021-03-21 10:01:20 +0900

    회원가입 화면 추가

---
+++ app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/SignUpActivity.kt
@@ -0,0 +1,23 @@
+package com.tjoeun.serverapp_daily10minutes_20210314
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
