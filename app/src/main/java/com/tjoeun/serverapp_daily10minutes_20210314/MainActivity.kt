d0d24a9a39e5612a9b88f3ea55308c391b2a106e
조경진 <cho881020@gmail.com>
2021-03-21 14:08:25 +0900

    JSONArray 추출 => 내부 데이터 확인해야함. (반복문 활용 필요)

--- app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/MainActivity.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/MainActivity.kt
@@ -3,6 +3,8 @@ package com.tjoeun.serverapp_daily10minutes_20210314
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.tjoeun.serverapp_daily10minutes_20210314.datas.Project
+import com.tjoeun.serverapp_daily10minutes_20210314.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -24,7 +26,21 @@ class MainActivity : BaseActivity() {
 //        메인화면에 들어오면 => 프로젝트 목록이 뭐뭐있는지 서버에 요청 (ServerUtil 함수 추가)
 //        받아온 결과를 분석해서 => Project() 형태로 만들어서 => mProjectList에 add해주자.
 
+        ServerUtil.getRequestProjectList(object : ServerUtil.JsonResponseHandler {
 
+            override fun onResponse(json: JSONObject) {
+
+                val dataObj = json.getJSONObject("data")
+
+//                data 내부의 [  ] 배열을 가져오는 코드.
+                val projectsArr = dataObj.getJSONArray("projects")
+
+//                projects [  ]  => 이름표가 아니라, 순서대로 하나씩 추출.
+//                첫번째 ~ 마지막까지 반복적으로 하나씩 추출. => Kotlin 반복문 활용.
+
+            }
+
+        } )
 
 
     }
