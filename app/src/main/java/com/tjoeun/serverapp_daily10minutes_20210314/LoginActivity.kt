4c6f2b83afde5cbb1d9d981ac78782c03c2fc53f
조경진 <cho881020@gmail.com>
2021-03-21 12:28:00 +0900

    MainActivity > LoginActivity로 변경

--- app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/LoginActivity.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/LoginActivity.kt
@@ -1,19 +1,18 @@
 package com.tjoeun.serverapp_daily10minutes_20210314
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.tjoeun.serverapp_daily10minutes_20210314.utils.ServerUtil
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_login.*
 import org.json.JSONObject
 
-class MainActivity : BaseActivity() {
+class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         setupEvents()
         setValues()
     }
