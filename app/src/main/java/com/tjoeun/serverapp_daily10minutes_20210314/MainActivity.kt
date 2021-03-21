4eafdd343505a73cbe1f7f0d5b8bf02887e1e3c8
조경진 <cho881020@gmail.com>
2021-03-21 09:41:30 +0900

    서버가 준 Json의 일부 추출 => 데이터로 가공 (UI 미반영)

--- app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/MainActivity.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/MainActivity.kt
@@ -2,6 +2,7 @@ package com.tjoeun.serverapp_daily10minutes_20210314
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.tjoeun.serverapp_daily10minutes_20210314.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
@@ -26,8 +27,23 @@ class MainActivity : BaseActivity() {
 //            이 아이디/비번이 회원이 맞는지, 서버에 확인 요청. => 로그인 요청.
 
             ServerUtil.postRequestLogin(inputId, inputPw, object : ServerUtil.JsonResponseHandler {
+
                 override fun onResponse(json: JSONObject) {
 //                    JSON 파싱 -> UI 반영 코드 작성 영역
+
+                    Log.d("화면입장", json.toString())
+
+                    val code = json.getInt("code")
+
+//                    서버 / 앱 약속 : code 가 200이면 로그인 성공. 그외 모든 값 로그인 실패
+
+                    if (code == 200) {
+//                        로그인 성공시 처리. => 향후 진행
+                    }
+                    else {
+//                        실패 처리. => 서버가 알려주는 실패사유를 토스트로 띄워보자. (UI반영)
+                    }
+
                 }
 
             })
