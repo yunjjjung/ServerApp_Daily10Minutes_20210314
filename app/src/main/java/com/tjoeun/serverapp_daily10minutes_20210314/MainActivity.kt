8ce98c72e394807a3abc6a62068def1bf0527fc4
조경진 <cho881020@gmail.com>
2021-03-21 14:51:29 +0900

    어댑터 객체 생성 > 리스트뷰와 연결  - 화면에 리스트가 안나오는 상태.

--- app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/MainActivity.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/MainActivity.kt
@@ -3,13 +3,17 @@ package com.tjoeun.serverapp_daily10minutes_20210314
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.tjoeun.serverapp_daily10minutes_20210314.adapters.ProjectAdapter
 import com.tjoeun.serverapp_daily10minutes_20210314.datas.Project
 import com.tjoeun.serverapp_daily10minutes_20210314.utils.ServerUtil
+import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
     val mProjectList = ArrayList<Project>()
+
+    lateinit var mAdapter : ProjectAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +27,10 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        mAdapter = ProjectAdapter(mContext, R.layout.project_list_item, mProjectList)
+
+        projectListView.adapter = mAdapter
 
 //        메인화면에 들어오면 => 프로젝트 목록이 뭐뭐있는지 서버에 요청 (ServerUtil 함수 추가)
 //        받아온 결과를 분석해서 => Project() 형태로 만들어서 => mProjectList에 add해주자.
