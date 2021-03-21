f8629bb87b818771b8f022f4736518b522d2a6f0
조경진 <cho881020@gmail.com>
2021-03-21 11:10:23 +0900

    회원가입 성공/실패시 UI 처리 진행중   - 가입 성공시, 그 사람 닉네임 출력 작업 진행 예정

--- app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/SignUpActivity.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/SignUpActivity.kt
@@ -2,6 +2,7 @@ package com.tjoeun.serverapp_daily10minutes_20210314
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.tjoeun.serverapp_daily10minutes_20210314.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import org.json.JSONObject
@@ -31,6 +32,29 @@ class SignUpActivity : BaseActivity() {
                 override fun onResponse(json: JSONObject) {
 
 //                    회원가입 결과에 따른 UI 반영 필요.
+//                    code : 200이면 가입성공 -> 토스트 + 회원가입 화면 종료.
+//                    그외 값 : 서버가 주는 실패 사유를 -> 토스트
+
+                    val code = json.getInt("code")
+
+                    runOnUiThread {
+
+                        if (code == 200) {
+//                            가입한 사람의 이름을 추출해서, 환영 메세지
+                            Toast.makeText(mContext, "??님 환영합니다.", Toast.LENGTH_SHORT).show()
+
+                            finish()
+                        } else {
+
+//                            서버가 주는 message 스트링을 토스트로 출력.
+                            val message = json.getString("message")
+
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+
+                        }
+
+                    }
+
 
                 }
 
