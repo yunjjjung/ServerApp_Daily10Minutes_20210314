5f26eccaed00e07a839d3e480b60d8b687ea6017
조경진 <cho881020@gmail.com>
2021-03-21 13:39:49 +0900

    참여프로젝트 정보 표현 클래스 제작

---
+++ app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/datas/Project.kt
@@ -0,0 +1,9 @@
+package com.tjoeun.serverapp_daily10minutes_20210314.datas
+
+class Project {
+
+    var id = 0 // Int라는 명시.
+    var title = "" // String 이라는 명시.
+    var imageURL = "" // 그림파일 경로 (String) 저장 변수.
+
+}
