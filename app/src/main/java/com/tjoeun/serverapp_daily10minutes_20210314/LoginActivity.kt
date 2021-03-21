15600a07eafe1c5d978ea5bc9508c71682ed3e5c
조경진 <cho881020@gmail.com>
2021-03-21 16:07:25 +0900

    로그인 성공시 > 서버가 주는 토큰값 추출

--- app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/LoginActivity.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/LoginActivity.kt
@@ -70,6 +70,13 @@ class LoginActivity : BaseActivity() {
                     if (code == 200) {
 //                        로그인 성공시 처리. => 메인화면 진입, 로그인화면 종료
 
+//                        토큰값이 뭔지 추출해서 => SharedPrefernces에 저장.
+
+                        val dataObj = json.getJSONObject("data")
+                        val token = dataObj.getString("token")
+
+
+
                         val myIntent = Intent(mContext, MainActivity::class.java)
                         startActivity(myIntent)
 
