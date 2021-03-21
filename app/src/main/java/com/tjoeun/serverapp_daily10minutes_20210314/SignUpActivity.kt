2e6fcd9973c98cdbb9616cda9d9fffac381c5582
조경진 <cho881020@gmail.com>
2021-03-21 10:22:32 +0900

    회원가입 클릭시 입력 데이터 받아오기

--- app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/SignUpActivity.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/SignUpActivity.kt
@@ -2,6 +2,7 @@ package com.tjoeun.serverapp_daily10minutes_20210314
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : BaseActivity() {
 
@@ -13,6 +14,19 @@ class SignUpActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        signUpBtn.setOnClickListener {
+
+//            이메일 / 비번 / 닉네임 -> 서버 회원가입기능에 전송.
+
+            val email = emailEdt.text.toString()
+            val pw = pwEdt.text.toString()
+            val nick = nicknameEdt.text.toString()
+
+//            서버 - 회원가입기능에 전송 : ServerUtil에 회원가입 함수 필요
+
+
+        }
 
     }
 
