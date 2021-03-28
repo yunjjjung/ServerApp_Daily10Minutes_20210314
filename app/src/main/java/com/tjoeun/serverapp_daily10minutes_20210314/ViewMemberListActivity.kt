1daba2acc376378457ae58f04825ad42ef24497f
조경진 <cho881020@gmail.com>
2021-03-28 15:49:41 +0900

    어댑터 객체 생성 - 리스트뷰와 연결  - 비동기 개념 활용 : 리스트뷰 새로 고침 코드 추가

--- app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/ViewMemberListActivity.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/ViewMemberListActivity.kt
@@ -2,9 +2,11 @@ package com.tjoeun.serverapp_daily10minutes_20210314
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tjoeun.serverapp_daily10minutes_20210314.adapters.ProjectMemberAdapter
 import com.tjoeun.serverapp_daily10minutes_20210314.datas.Project
 import com.tjoeun.serverapp_daily10minutes_20210314.datas.User
 import com.tjoeun.serverapp_daily10minutes_20210314.utils.ServerUtil
+import kotlinx.android.synthetic.main.activity_view_member_list.*
 import org.json.JSONObject
 
 class ViewMemberListActivity : BaseActivity() {
@@ -12,6 +14,8 @@ class ViewMemberListActivity : BaseActivity() {
     lateinit var mProject : Project
 
     val mMemberList = ArrayList<User>()
+
+    lateinit var mMemberAdapter : ProjectMemberAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,9 +50,22 @@ class ViewMemberListActivity : BaseActivity() {
 
                 }
 
+//                이 코드는 비동기로 돌아가는 영역.
+//                하단의 어댑터 연결코드가 => 상단이 있는 멤버데이터추가보다, 먼저 완료될 수 도 있다.
+//                멤버목록이 다 추가되면 => 어댑터 새로고침 시켜주자.
+
+                runOnUiThread {
+                    mMemberAdapter.notifyDataSetChanged()
+                }
+
+
+
             }
 
         })
+
+        mMemberAdapter = ProjectMemberAdapter(mContext, R.layout.member_list_item, mMemberList)
+        memberListView.adapter = mMemberAdapter
 
     }
 
