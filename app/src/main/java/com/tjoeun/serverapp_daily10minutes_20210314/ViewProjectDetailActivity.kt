e9be2d079bf2d789afe14bf0deba3d48f26a8379
조경진 <cho881020@gmail.com>
2021-03-28 11:23:55 +0900

    참여 신청 기능 구현 -> 실패 응답만 우선 처리

--- app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/ViewProjectDetailActivity.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/ViewProjectDetailActivity.kt
@@ -2,9 +2,12 @@ package com.tjoeun.serverapp_daily10minutes_20210314
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.tjoeun.serverapp_daily10minutes_20210314.datas.Project
+import com.tjoeun.serverapp_daily10minutes_20210314.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_view_project_detail.*
+import org.json.JSONObject
 
 class ViewProjectDetailActivity : BaseActivity() {
 
@@ -23,6 +26,44 @@ class ViewProjectDetailActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+//        참가신청 버튼이 눌리면 => 신청 API 호출.
+
+        applyBtn.setOnClickListener {
+
+            ServerUtil.postRequestApplyProject(
+                mContext,
+                mProject.id,
+                object : ServerUtil.JsonResponseHandler {
+                    override fun onResponse(json: JSONObject) {
+
+//                    신청 결과에 따른 처리.
+//                    code : 200 => 신청 성공, 그외 값 : 신청 실패. message : 실패사유String
+//                    400 - 잘못된 데이터 유입.
+//                    403 - 권한이 없는데 요청. ex. 관리자 전용 기능 => 일반회원이 요청.
+//                    404 - /user, /project 등 기능 주소를 잘못 입력한 경우. (Not found)
+//                    500 - 서버 내부의 로직 에러.
+
+                        val code = json.getInt("code")
+
+                        if (code == 200) {
+//                        정상 신청 완료
+                        } else {
+//                        여타 이유로 실패.
+                            val message = json.getString("message")
+
+                            runOnUiThread {
+                                Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                            }
+
+                        }
+
+
+                    }
+
+                })
+
+        }
 
     }
 
