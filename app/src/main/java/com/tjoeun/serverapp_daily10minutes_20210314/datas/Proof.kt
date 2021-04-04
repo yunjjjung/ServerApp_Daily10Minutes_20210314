3e93f35a3a9db6ca8063e24c553db20b662ebcaf
조경진 <cho881020@gmail.com>
2021-04-04 14:39:09 +0900

    인증글 작성자 정보까지 한번에 파싱.  - User클래스에 만들어둔 기능 재활용

--- app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/datas/Proof.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/datas/Proof.kt
@@ -19,6 +19,15 @@ class Proof {
             proof.id = jsonObj.getInt("id")
             proof.content = jsonObj.getString("content")
 
+//            게시글의 작성자?
+//            User 형태 => 적당한 JSON을 넣으면 => User로 바꿔주는 기능.
+//            만들어둔걸 활용하자.!
+
+//            user 정보를 갖고있는 JSON 추출
+            val userObj = jsonObj.getJSONObject("user")
+
+            proof.writer = User.getUserDataFromJson(userObj)
+
             return proof
         }
 
