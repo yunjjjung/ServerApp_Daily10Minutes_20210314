2b34a86ab504b433a7a9a382de2a5ec9992b92bb
조경진 <cho881020@gmail.com>
2021-04-04 15:10:41 +0900

    서버가 주는 인증글 작성 시점 파싱  - SimpleDateFormat : parse 기능 활용 예시

--- app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/datas/Proof.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/datas/Proof.kt
@@ -1,6 +1,8 @@
 package com.tjoeun.serverapp_daily10minutes_20210314.datas
 
 import org.json.JSONObject
+import java.text.SimpleDateFormat
+import java.util.*
 
 class Proof {
 
@@ -9,6 +11,11 @@ class Proof {
 
 //    인증글의 하위정보로 > 누가 작성했는지 User를 담아두자.
     lateinit var writer : User
+
+//    이 인증글이 언제 적혔는지? Calendar형태로 보관하자.
+//    나중에 상황에 따라 SimpleDateFormat으로 가공해서 사용하기 위함.
+
+    val proofDateTime = Calendar.getInstance()
 
     companion object {
 
@@ -27,6 +34,15 @@ class Proof {
             val userObj = jsonObj.getJSONObject("user")
 
             proof.writer = User.getUserDataFromJson(userObj)
+
+//            이 글이 언제 적혔는지? 서버가 주는 정보를 반영.
+//            서버가 주는 String => (SimpleDateFormat활용)분석 => Calendar로 변환.
+
+            val proofTimeStr = jsonObj.getString("proof_time")
+
+            val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+            proof.proofDateTime.time = sdf.parse(proofTimeStr)
+
 
             return proof
         }
