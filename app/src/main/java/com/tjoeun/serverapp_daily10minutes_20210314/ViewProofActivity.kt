8586054d359b3322709c94bfd34b40b7275158e1
조경진 <cho881020@gmail.com>
2021-04-04 12:17:10 +0900

    날짜 선택 팝업창 띄우기 (기초작업만)

--- app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/ViewProofActivity.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/ViewProofActivity.kt
@@ -1,7 +1,10 @@
 package com.tjoeun.serverapp_daily10minutes_20210314
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.DatePicker
+import kotlinx.android.synthetic.main.activity_view_proof.*
 
 class ViewProofActivity : BaseActivity() {
 
@@ -13,6 +16,30 @@ class ViewProofActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+//        날짜 선택 버튼이 눌리면 => 달력을 띄우자.
+
+        selectDateBtn.setOnClickListener {
+
+//            1. 날짜가 선택이 되면 => 어떤 행동을 할지 가이드북 변수.
+
+            val dateSetListener = object : DatePickerDialog.OnDateSetListener {
+                override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+//                    연 / 월 / 일을 가지고 처리할 코드 작성.
+
+                }
+
+            }
+
+//            2. 그 가이드북을 들고, 실제로 날짜를 선택하러 가기. (날짜 선택용 팝업창 띄우기)
+
+            val datePickerDialog = DatePickerDialog(mContext, dateSetListener,
+                2021, 4, 4)
+
+            datePickerDialog.show()
+
+        }
 
     }
 
